ca8ad5393d5cc54f2faf8ee4ecf9ecb0742fd8d2
Etanami Olatunji <etanamiolatunji@gmail.com>
2022-10-30 23:53:12 -0700

    style: link section component

--- src/components/LinkSection.js
+++ src/components/LinkSection.js
@@ -4,26 +4,32 @@ const LinkSection =() => {
     <section>
       <a 
         href="https://twitter.com/etanamie" 
+        id="btn__twitter"
       >Twitter Link
       </a>
       <a 
         href="https://training.zuri.team" 
+        id="btn__zuri" 
       >Zuri Team
       </a>
       <a 
         href="https://books.zuri.team" 
+        id="books"
       >Zuri Books
       </a>
       <a 
         href="https://training.zuri.team/python-for-beginners?ref_id=etanamie" 
+        id="book__python" 
       >Python Books
       </a>
       <a 
         href="https://background.zuri.team" 
+        id="pitch"
       >Background Check for Coders
       </a>
       <a 
         href="https://books.zuri.team/design-rules" 
+        id="book__design" 
       >Design Bokks
       </a>
     </section>
